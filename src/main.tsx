de01d1125035fb11454b7e64646bde63b515cc63
sharone2012 <62740676+sharone2012@users.noreply.github.com>
2026-03-30 11:42:57 +0300

    Wrap app with MillProvider for S0-S6 shared state

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MillProvider } from "./contexts/MillContext";
 import App from "./App.jsx";
 import LabAnalysisV2 from "./pages/LabAnalysisV2.jsx";
 import S3Landing from "./pages/S3Landing.jsx";
@@ -19,23 +20,25 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/lab" element={<LabAnalysisV2 />} />
-      <Route path="/s3" element={<S3Landing />} />
-      <Route path="/s3/a-bio-library" element={<S3ABioLibrary />} />
-      <Route path="/s1-index" element={<S1Index />} />
-      <Route path="/S1-index" element={<S1Index />} />
-      <Route path="/s1-capex-opex" element={<S1CapexOpex />} />
-      <Route path="/s1-efb-ascii" element={<S1EfbAscii />} />
-      <Route path="/s1-opdc-ascii" element={<S1OpdcAscii />} />
-      <Route path="/s1-pos-ascii" element={<S1PosAscii />} />
-      <Route path="/s1-efb-spec" element={<S1EfbSpec />} />
-      <Route path="/s1-opdc-spec" element={<S1OpdcSpec />} />
-      <Route path="/s1-pos-spec" element={<S1PosSpec />} />
-      <Route path="/s1-combined" element={<S1Combined />} />
-      <Route path="/s1-engineering" element={<S1Engineering />} />
-      <Route path="/CFI_AG_Management_Planning_Calculator" element={<CFI_AG_Management_Planning_Calculator />} />
-    </Routes>
+    <MillProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/lab" element={<LabAnalysisV2 />} />
+        <Route path="/s3" element={<S3Landing />} />
+        <Route path="/s3/a-bio-library" element={<S3ABioLibrary />} />
+        <Route path="/s1-index" element={<S1Index />} />
+        <Route path="/S1-index" element={<S1Index />} />
+        <Route path="/s1-capex-opex" element={<S1CapexOpex />} />
+        <Route path="/s1-efb-ascii" element={<S1EfbAscii />} />
+        <Route path="/s1-opdc-ascii" element={<S1OpdcAscii />} />
+        <Route path="/s1-pos-ascii" element={<S1PosAscii />} />
+        <Route path="/s1-efb-spec" element={<S1EfbSpec />} />
+        <Route path="/s1-opdc-spec" element={<S1OpdcSpec />} />
+        <Route path="/s1-pos-spec" element={<S1PosSpec />} />
+        <Route path="/s1-combined" element={<S1Combined />} />
+        <Route path="/s1-engineering" element={<S1Engineering />} />
+        <Route path="/CFI_AG_Management_Planning_Calculator" element={<CFI_AG_Management_Planning_Calculator />} />
+      </Routes>
+    </MillProvider>
   </BrowserRouter>
 );
