99b2d5506d0ee9461543e70a1ecbade48ee745df
sharone2012 <62740676+sharone2012@users.noreply.github.com>
2026-03-30 11:54:52 +0300

    Add S3Biological component to s3-entry.jsx

---
+++ src/s3-entry.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import S3Biological from "./pages/S3Biological";
+
+ReactDOM.createRoot(document.getElementById("s3-root")).render(
+  <React.StrictMode>
+    <S3Biological />
+  </React.StrictMode>
+);
