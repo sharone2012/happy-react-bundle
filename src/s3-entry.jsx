84260f744fd3f09cc54f7d68ed1044e57d2a3079
sharone2012 <62740676+sharone2012@users.noreply.github.com>
2026-03-30 13:00:26 +0300

    Update s3-entry.jsx

--- src/s3-entry.jsx
+++ src/s3-entry.jsx
@@ -7,12 +7,3 @@ ReactDOM.createRoot(document.getElementById("s3-root")).render(
     <BioProcessing />
   </React.StrictMode>
 );
-import React from "react";
-import ReactDOM from "react-dom/client";
-import S3Biological from "./pages/S3Biological";
-
-ReactDOM.createRoot(document.getElementById("s3-root")).render(
-  <React.StrictMode>
-    <S3Biological />
-  </React.StrictMode>
-);
